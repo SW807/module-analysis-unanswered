b0db8467abf366da2ff4ac7f8db87a4fbb6b001d
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-24 10:05:01 +0100

    extends scheduledservice and implemented its methods

--- app/src/main/java/dk/aau/cs/psylog/unansweredcalls/PsyLogService.java
+++ app/src/main/java/dk/aau/cs/psylog/unansweredcalls/PsyLogService.java
@@ -1,4 +1,15 @@
 package dk.aau.cs.psylog.unansweredcalls;
 
-public class PsyLogService {
+import dk.aau.cs.psylog.module_lib.ScheduledService;
+
+public class PsyLogService extends ScheduledService{
+
+    public PsyLogService() {
+        super("PsyLogIntentService-UnansweredCalls");
+    }
+
+    @Override
+    public void setScheduledTask() {
+        scheduledTask = new UnansweredCallsListener(this);
+    }
 }
